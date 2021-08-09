fac636b2946a4e86bc1185a4732d47943233ace8
Irene Yang <irene970617@gmail.com>
2021-08-09 11:56:44 +0800

    try dbUrl

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ const campgroundRoutes = require('./routes/campground');
 const reviewRoutes = require('./routes/reviews');
 const userRoutes = require('./routes/user');
 const MongoStore = require("connect-mongo");
-const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp'
+const dbUrl = process.env.DB_URL
 //const dbUrl = process.env.DB_URL;
 
 
